523baad8dbe70d5e18ff0d8f40ca96e29dbe7245
Hamza hsaina <hhsaina4@gmail.com>
2025-01-31 17:15:30 +0100

    Test Efm

--- src/Ex1/TestApi.js
+++ src/Ex1/TestApi.js
@@ -1,5 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 
 
-export default function 
+export default function Api(){
+    const [cours,setcours] = useState('')
+
+
+    useEffect{
+        axios.get(' https://www.robotstxt.org/robotstxt.html');
+        .then((A)=>setcours(A.data))
+
+    }
+}
